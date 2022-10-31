9e020305dfa3d460713f3feed0f3cb0fd06ab681
Bella Irene <bellagarcia1985@gmail.com>
2022-10-31 10:36:43 +0100

    include bad army

--- js/main.js
+++ js/main.js
@@ -9,19 +9,41 @@ const select = document.querySelector('.js-select');
 function getRandom(max) {
     return Math.ceil(Math.random() * max);
   }
+function wrong () {
+  debugger
+  let wrongRandon = getRandom (5);
+  let  strong = ""
+  if ( wrongRandon === 1 ){ 
+    strong = 2
+  }
+ if (wrongRandon === 2 ){ 
+strong = 2 
+}
+if (wrongRandon === 3 ){ 
+  strong = 2 
+  }
+if (wrongRandon === 4 ){ 
+strong = 3 
+    }
+ if (wrongRandon === 5 ){ 
+ strong = 5
+      }
+     
+      return strong ;
+    };
 
 
 function selectGood () {
-  debugger
-  let wrong = getRandom (6);
+ 
+  let strong =  wrong();
  let good = parseInt(select.value)
-if (good > wrong) {
+if (good > strong) {
   result.innerHTML = " Ha ganado el Ejército del Bien! Enhorabuena."
 }
- if (good < wrong) {
+ if (good < strong) {
   result.innerHTML = " Ha ganado el Ejército del Mal! Vuelve a Intentarlo."
  }
- if (good === wrong ) {
+ if (good === strong ) {
   result.innerHTML = "Empate."
  }
 }
