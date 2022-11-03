7017fd024de15476f52cfa2ad61e51f6312093df
Bella Irene <bellagarcia1985@gmail.com>
2022-11-03 22:19:48 +0100

    improvements made in the absence of advanced js issus

--- js/main.js
+++ js/main.js
@@ -5,6 +5,11 @@ const btn = document.querySelector('.js-btn');
 const select = document.querySelector('.js-select');
 const resutJugador = document.querySelector('.js-result-jugador');
 const resutComputadora = document.querySelector('.js-result-computadora');
+const btnReset = document.querySelector('.js-btn2');
+const end = document.querySelector('.js-end');
+let goodFinal = 0;
+let strongFinal = 0;
+let partidas = 0;
 
 function getRandom(max) {
   return Math.ceil(Math.random() * max);
@@ -30,23 +35,46 @@ function wrong() {
 
   return strong;
 }
+
 function selectGood() {
   let strong = wrong();
   let good = parseInt(select.value);
   if (good > strong) {
     result.innerHTML = ' Ha ganado el Ejército del Bien! Enhorabuena.';
-    paintJugador.innerHTML = '1';
+    goodFinal++;
+    partidas++;
+    resutJugador.innerHTML = `${goodFinal}`;
   }
   if (good < strong) {
     result.innerHTML = ' Ha ganado el Ejército del Mal! Vuelve a Intentarlo.';
-    paintComputadora.innerHTML = '1';
-  } else if (good === strong) {
+    strongFinal++;
+    partidas++;
+    resutComputadora.innerHTML = `${strongFinal}`;
+  }
+  else if (good === strong) {
     result.innerHTML = 'Empate.';
+    partidas++;
+  }
+}
+function point() {
+  if (partidas === 10) {
+    btnReset.classList.remove('reinicio');
+    btn.classList.add('reinicio');
+    endF();
+  }
+}
+function endF() {
+  if (goodFinal > strongFinal) {
+    end.innerHTML = ' Has ganado el juego!';
+  } else if (goodFinal < strongFinal) {
+    end.innerHTML = ' El ordenador ganado el juego!';
+  } else {
+    end.innerHTML = 'Empate!';
   }
 }
 function handleClick(e) {
   e.preventDefault();
   selectGood();
+  point();
 }
-
 btn.addEventListener('click', handleClick);
