824060ada26337bb4567d5b3827ebaf904ac0e10
Bella Irene <bellagarcia1985@gmail.com>
2022-11-08 13:53:55 +0100

    change finish

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@
 const result = document.querySelector('.js-result');
 const btn = document.querySelector('.js-btn');
 const select = document.querySelector('.js-select');
-const resutJugador = document.querySelector('.js-result-jugador');
-const resutComputadora = document.querySelector('.js-result-computadora');
+const resultJugador = document.querySelector('.js-result-jugador');
+const resultComputadora = document.querySelector('.js-result-computadora');
 const btnReset = document.querySelector('.js-btn2');
 const end = document.querySelector('.js-end');
 let goodFinal = 0;
@@ -35,28 +35,30 @@ painthtml.innerHTML = write ;
 function selectGood() {
   let strong = wrong();
   let good = parseInt(select.value);
-  if (good === x) {
+  if (good === 0) {
     paint (result, ' Debe elegir una raza.' ); }
-  if (good > strong) {
+   else { if (good > strong) {
     paint (result, ' Ha ganado el Ejército del Bien! Enhorabuena.' );
     // result.innerHTML = ' Ha ganado el Ejército del Bien! Enhorabuena.'; este si funciona
     goodFinal++;
     movie++;
-    paint (resultJugador, `${goodFinal}`);
+    paint (resultJugador, goodFinal);
     // resutJugador.innerHTML = `${goodFinal}`;  preguntsar por que no funciona asi
   }
   if (good < strong) {
     paint (result, ' Ha ganado el Ejército del Mal! Vuelve a Intentarlo.' );
-    // result.innerHTML = ' Ha ganado el Ejército del Mal! Vuelve a Intentarlo.';
     strongFinal++;
     movie++;
-    resutComputadora.innerHTML = `${strongFinal}`;
+    paint (resultComputadora, strongFinal);
   }
   else if (good === strong) {
     paint (result, 'Empate.' );
-    // result.innerHTML = 'Empate.';
+   
     movie++;
   }
+
+    
+   } 
 }
 function point() {
   if (movie === 10) {
